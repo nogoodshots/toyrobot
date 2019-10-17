ac47587c9f466e8d264fa4074fa0baeec874c6f8
100 Trees <100trees.com@gmail.com>
2019-10-16 21:44:43 -0700

    Project creation

---
+++ src/main/java/net/nogoodshots/toyrobot/App.java
@@ -0,0 +1,13 @@
+package net.nogoodshots.toyrobot;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/net/nogoodshots/toyrobot/AppTest.java
@@ -0,0 +1,20 @@
+package net.nogoodshots.toyrobot;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
